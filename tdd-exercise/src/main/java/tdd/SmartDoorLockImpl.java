f7e6296f91ba36781c1dd9ea0e77c03b003eebd6
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2025-02-24 11:24:21 +0100

    Exercise 2 Implemented test and passed for isLocked

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,48 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+
+    public SmartDoorLockImpl() {
+
+    }
+
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private SmartDoorLock smartDoorLock = new SmartDoorLockImpl();
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testLock() {
+        assertTrue(smartDoorLock.isLocked());
     }
+
+
 }
